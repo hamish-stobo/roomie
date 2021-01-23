17eb87c195287326599fd4043c0f637eda2e5908
Jared-James <jaredjames.personal@gmail.com>
2021-01-24 11:12:35 +1300

    setup server

---
+++ src/server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path'); 
+
+const app = express();
+const port = process.env.PORT || 3000;
+const DIST_DIR = path.join(__dirname, '../', '../dist'); 
+const HTML_FILE = path.join(DIST_DIR, 'index.html'); 
+const mockResponse = {
+  foo: 'bar',
+  bar: 'foo'
+};
+app.use(express.static(DIST_DIR));
+app.get('/api', (req, res) => {
+  res.send(mockResponse);
+});
+app.get('/', (req, res) => {
+ res.sendFile(HTML_FILE);
+});
+app.listen(port, function () {
+ console.log('App listening on port: ' + port);
+});
