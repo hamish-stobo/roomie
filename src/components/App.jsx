85f38f224d42bc8d5b316a9d107086a710ad23f0
Jared-James <jaredjames.personal@gmail.com>
2021-01-24 10:53:22 +1300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/styles'
+
+const App = () => {
+  return (
+    <>
+    <h3>ROOOOOOOOOMIE</h3>
+    </>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+   context: __dirname,
+   entry: './src/index.js',
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+      publicPath: '/',
+   },
+   devServer: {
+      historyApiFallback: true,
+      inline: true
+   },
+   module: {
+    rules: [
+       {
+          test: /\.js$|jsx/,
+          use: 'babel-loader',
+       },
+       {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+       },
+       {
+          test: /\.(png|j?g|svg|gif)?$/,
+          use: 'file-loader'
+       }
+    ]
+ },
+
+   resolve: { extensions: ["*", ".js", ".jsx", '.scss'] },
+   plugins: [
+      new HtmlWebPackPlugin({
+         inject: true,
+         template: path.resolve(__dirname, 'public/index.html'),
+         filename: 'index.html'
+      }),
+   ]
+};
+
+
+
