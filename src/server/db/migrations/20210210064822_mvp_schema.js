d6bd5ce2be9a01003a2024e8c74e101a94dc295f
HHSNZ <hh.stobo@gmail.com>
2021-02-10 07:19:04 +1300

    refactored migrations so they run in correct order. Working on seed files to insert mock data passing keys down to foreign key fields where needed.

---
+++ src/server/db/migrations/20210210064822_mvp_schema.js
@@ -0,0 +1,42 @@
+
+exports.up = knex => {
+    return knex.schema.createTable('users', table => {
+        table.uuid('id').unique().notNullable()
+        table.string('email').unique().notNullable()
+        table.string('first_name').notNullable()
+        table.string('last_name').notNullable()
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+      })
+      .createTable('advertisements', table => {
+        table.uuid('id').unique().notNullable()
+        table.uuid('user_id').unique().notNullable()
+        table.foreign('user_id').references('id').inTable('users')
+        table.float('rent', { precision: 2 }).notNullable()
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+      })
+      .createTable('location', table => {
+        table.uuid('id').unique().notNullable()
+        table.string('suburb').notNullable()
+        table.integer('postcode').notNullable()
+        table.uuid('user_id')
+        table.foreign('user_id').references('id').inTable('users')
+        table.uuid('advertisement_id')
+        table.foreign('advertisement_id').references('id').inTable('advertisements')
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+    })
+    .createTable('interest', table => {
+        table.uuid('id').unique().notNullable()
+        table.uuid('user_id').notNullable()
+        table.foreign('user_id').references('users.id')
+        table.uuid('advertisement_id').notNullable()
+        table.foreign('advertisement_id').references('advertisements.id')
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+      })
+}
+
+exports.down = knex => {
+    return knex.schema.dropTableIfExists('interest')
+        .dropTableIfExists('location')
+        .dropTableIfExists('advertisements')
+        .dropTableIfExists('users')
+}
--- src/server/db/seeds/users.js
+++ src/server/db/seeds/users.js
@@ -1,13 +1,21 @@
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
 
 exports.seed = knex => {
   // Deletes ALL existing entries
   return knex('users').del()
-    .then(() => {
-      // Inserts seed entries
-      return knex('users').insert([
+    .then(() => knex('users').insert([
         {id: uuidv4(), email: 'hello@test1.com', first_name: 'John', last_name: 'Smith'},
         {id: uuidv4(), email: 'testemail@test2.com', first_name: 'Jane', last_name: 'Kelly'},
-        {id: uuidv4(), email: 'myemail@test1.com', first_name: 'Jeremy', last_name: 'Brown'}      ]);
-    });
-};
+        {id: uuidv4(), email: 'myemail@test1.com', first_name: 'Jeremy', last_name: 'Brown'}
+    ])
+    .then(users => {
+      return knex('advertisements').del()
+      .then(() => knex('advertisements').insert([
+        {id: uuidv4(), user_id: users[0].id, rent: Math.round(Math.random() * 200)},
+        {id: uuidv4(), user_id: users[1].id, rent: Math.round(Math.random() * 200)},
+        {id: uuidv4(), user_id: users[2].id, rent: Math.round(Math.random() * 200)}
+      ])
+      )
+    })
+    )
+  }
