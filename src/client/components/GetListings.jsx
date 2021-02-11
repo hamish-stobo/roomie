3258e45f4956f7fff0195483292d330202365534
jared james <jaredjames.personal@gmail.com>
2021-02-12 12:03:13 +1300

    now pulling api data in

--- src/client/components/GetListings.jsx
+++ src/client/components/GetListings.jsx
@@ -7,17 +7,17 @@ import Loading from './Loading'
 
 const GetListings = () => {
     const [users, setUsers] = useState([])
-
+    // This will need to be changed to /getListings when it's ready
     const getUser = async () => {
         const { data } = await axios.get('/getUsers');
         setUsers(data)
     }
-
+    // on load || change in users.length getUser will make the axios call and return the users
     useEffect(() => {
         getUser()
     }, [users.length])
 
-
+ 
     return (
         <>
             <div class="GetListings">
---
+++ src/client/components/SignUpForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SignUpForm = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default SignUpForm
