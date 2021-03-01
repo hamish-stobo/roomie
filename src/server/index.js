48e3eee9b46964f7ccd5b326df98ee496e3558f4
HHSNZ <hh.stobo@gmail.com>
2021-03-01 20:03:09 +1300

    minor change to server's index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,7 @@
 const app = require('./app')
 const port = process.env.PORT || 6969
 
-const storeFieldsInfo = require('./validation/storeFieldsInfo')
-
 app.listen(port, async function () {
- await storeFieldsInfo(['users', 'listings', 'locations', 'likes'])
  console.log(`🚀🚀🚀 App listening on port: ${port} 🚀🚀🚀`);
  console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
 });
