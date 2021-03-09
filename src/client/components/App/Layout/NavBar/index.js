a595039d5396cf185303adf8ebabf9922f5481b0
HHSNZ <hh.stobo@gmail.com>
2021-03-10 05:54:07 +1300

    Navbar v1 looking gooood

--- src/client/components/App/Layout/NavBar/index.js
+++ src/client/components/App/Layout/NavBar/index.js
@@ -11,11 +11,24 @@ const NavBar = () => {
     return (
         <>
             <div className="NavBar">
-                <div className="Logo lowercase">NavBar</div>
-                <div className="Hamburger sm" onClick={() => toggleShow(!show)}>Hamburger</div>
+                <div className="Logo lowercase">rOOmie</div>
+                <div className="Hamburger sm" onClick={() => toggleShow(!show)}>
+                {!show
+                ?
+                <svg width="35" height="35" viewBox="0 0 43 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <line x1="3" y1="3" x2="40" y2="3" stroke="#DB00FF" stroke-opacity="0.74" stroke-width="6" stroke-linecap="round"/>
+                    <line x1="3" y1="15" x2="40" y2="15" stroke="#DB00FF" stroke-opacity="0.74" stroke-width="6" stroke-linecap="round"/>
+                    <line x1="3" y1="27" x2="40" y2="27" stroke="#DB00FF" stroke-opacity="0.74" stroke-width="6" stroke-linecap="round"/>
+                </svg>
+                : 
+                <svg width="30" height="30" viewBox="0 0 35 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <line x1="4.29718" y1="30.96" x2="30.4601" y2="4.79704" stroke="#DB00FF" stroke-opacity="0.74" stroke-width="6" stroke-linecap="round"/>
+                    <line x1="4.53982" y1="4.79688" x2="30.7028" y2="30.9598" stroke="#DB00FF" stroke-opacity="0.74" stroke-width="6" stroke-linecap="round"/>
+                </svg>
+                }
+                </div>
                 {show && 
                 <div className="menu-container sm">
-                    <button className="exitBtn exit-nav sm" onClick={() => toggleShow(false)}>X</button>
                     <ul className="menu-ul sm">
                         <li className="menu-li sm">Profile</li>
                         <li className="menu-li sm">List a Room</li>
