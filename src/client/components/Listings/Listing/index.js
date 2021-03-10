227180bfea98e3be1842019020bff49b5f0772a5
HHSNZ <hh.stobo@gmail.com>
2021-03-11 06:32:05 +1300

    gonna save before I mess everything up lol. Styling index page.

--- src/client/components/Listings/Listing/index.js
+++ src/client/components/Listings/Listing/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../../styles/styles'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
@@ -17,18 +17,32 @@ const Listing = ({listing}) => {
   //   //   listings_user_id: userId
   //   // })
   // }
+  const [selected, setSelected] = useState(0)
+  const imgsArr = new Array(5).fill("https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80")
+  const dotsArr = ["dot", "dot", "dot", "dot", "dot"]
 
   return (
     <div className="ListingContainer">
       <div className="cardTop">
-        <img className="profileImage" src="https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80" />
+        <img className="profileImage" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"/>
         <div className="nameDate">
           <span className="userName">John Smith</span>
           <span className="postedDate">Listed Today</span>
         </div>
       </div>
-      <div className="image-carousel">
-        
+      <div className="imageCarousel">
+        <img className="bedroomImage" src="https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
+        <div className="indexCount">1/5</div>
+      </div>
+      <div className="underImage">
+        <div className="Likes">
+          <span>19</span>
+          <div>LikesIcon</div>
+        </div> 
+        <div className="dots">
+          {dotsArr.map((item, idx) => <div className="dot" key={idx} > </div>)}
+        </div>
+        <div className="message">Message Seller</div>
       </div>
       {/* <h3>Seller's user ID: {listings_user_id}</h3>
       <ul>
