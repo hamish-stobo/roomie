14eba02e07707e628f20e417db491bbecf16cb19
HHSNZ <hh.stobo@gmail.com>
2021-03-14 17:11:18 +1300

    profile page looking decent

--- src/client/components/Profile/index.js
+++ src/client/components/Profile/index.js
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import '../../styles/styles'
 import Listings from '../Listings'
 
 const Profile = () => {
+  const [left, setLeft] = useState(true)
+  const changeSelected = input => {
+    setLeft(input)
+  }
   return (
     <>
-    <div className="profileContainer">
-      <img class="profileImg" src="https://images.unsplash.com/photo-1496345875659-11f7dd282d1d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80" />
-      <div className="profileDetailsColumn">
-        <span>John Smith</span>
-        <span>john.smith@gmail.com</span>
-        <span>+64210395224</span>
-        <span>Member since 05/02/2021</span>
+    <div className="profileWrapper">
+      <div className="profileContainer">
+        <img class="profileImg" src="https://images.unsplash.com/photo-1496345875659-11f7dd282d1d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80" />
+        <div className="profileDetailsColumn">
+          <span className="username">John Smith</span>
+          <span>john.smith@gmail.com</span>
+          <span>+64210395224</span>
+          <span>Member since 05/02/2021</span>
+        </div>
+        <Link to="/editprofile" className="button editButton">Edit Profile</Link>
       </div>
-      <div className="editButton">Edit Profile</div>
     </div>
-    <div className="LikesListingsButtons">
-      <div>Likes</div>
-      <div>Listings</div>
-    </div>
-    {/* <Listings /> */}
-    </>
+      <div className="LikesListingsButtons">
+        <div onClick={() => changeSelected(true)} className={left && 'selected'}>Likes</div>
+        <div onClick={() => changeSelected(false)} className={!left && 'selected'}>Your Listings</div>
+      </div>
+      {left 
+        ? <div>You haven't liked any listings yet!</div>
+        : <Listings />
+      }
+      </>
   )
 }
 
