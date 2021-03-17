b7fec105e2df4386cbe9f96f04926d587eb36bcf
HHSNZ <hh.stobo@gmail.com>
2021-03-18 06:58:28 +1300

    working on multi-file submission in add listing

--- src/client/components/AddListing/index.js
+++ src/client/components/AddListing/index.js
@@ -1,9 +1,11 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 import '../../styles/styles'
 
 
 const AddListing = () => {
     const [listingDetails, setListingDetails] = useState(new Map()) 
+    const [images, setImages] = useState({})
     const onChange = e => {
         const { name, value } = e.target
         //create a COPY of the existing map in state
@@ -11,10 +13,27 @@ const AddListing = () => {
         setListingDetails(new Map(listingDetails.set(name, value)))
         console.log(`name ${name}, value ${value}`)
     }
+    const addImages = e => {
+        setImages(images.images = e.target.files)
+        console.log(`images: ${images.images.length}`)
+    }
     const submit = e => {
         e.preventDefault()
-        const userObject = Object.fromEntries(listingDetails)
-        console.log(JSON.stringify(userObject))
+        const formData = new FormData();
+        listingDetails.forEach((val, key) => {
+            formData.append(key, val)
+        })
+        const imgsArr = images.images
+        for(let i = 0; i < imgsArr.length; i++) {
+            formData.append(`images${i}`, imgsArr[i])
+            console.log(JSON.stringify(imgsArr[i]))
+        }
+        console.log(JSON.stringify(formData))
+        // axios.post('/api/v1/listings', formData, {
+        //     headers: {
+        //         'content-type': 'multipart/form-data'
+        //     }
+        // })
     }
     return (
         <div className="AddListingContainer">
@@ -24,12 +43,12 @@ const AddListing = () => {
             <form className="Form addListingForm" onSubmit={submit}>
                 <input required className={`text-input ${!listingDetails.get('title') ? '' : 'lowercase'}`} type="text" name="title" placeholder="Title" value={listingDetails.get('title')} onChange={onChange} />
                 <input required className={`text-input ${!listingDetails.get('location') ? '' : 'lowercase'}`} type="text" name="location" placeholder="Location (suburb)" value={listingDetails.get('location')} onChange={onChange} />
-                <input required className={`text-input ${!listingDetails.get('rent') ? '' : 'lowercase'}`} type="text" name="rent" placeholder="Weekly rent" value={listingDetails.get('rent')} onChange={onChange} />
+                <input required className={`text-input ${!listingDetails.get('rent') ? '' : 'lowercase'}`} type="number" name="rent" placeholder="Weekly rent" value={listingDetails.get('rent')} onChange={onChange} />
                 <input required className="text-input" type="number" name="bedrooms" placeholder="No. of bedrooms:" value={listingDetails.get('bedrooms')} onChange={onChange} />
                 <input required className="text-input" type="number" name="bathrooms" placeholder="No. of bathrooms:" value={listingDetails.get('bathrooms')} onChange={onChange} />
                 <div className="text-input fileContainer">
                     <label htmlFor="file-upload-label">Upload one or more photos</label>
-                    <input required id="file-upload" className="fileUpload" type="file" multiple accept="image/png, image/jpeg" name="images" onChange={onChange}/>
+                    <input required id="file-upload" className="fileUpload" type="file" multiple accept="image/png, image/jpeg" name="images" onChange={addImages}/>
                 </div>
                 <input required className="button" value="Sign Up" type="submit" name="submit" />
             </form>
