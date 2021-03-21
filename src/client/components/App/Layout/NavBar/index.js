44c623ceaf7aff700505302f317252776356c993
HHSNZ <hh.stobo@gmail.com>
2021-03-22 06:53:54 +1300

    FE looking good, next up: imageupload to back end

--- src/client/components/App/Layout/NavBar/index.js
+++ src/client/components/App/Layout/NavBar/index.js
@@ -6,6 +6,7 @@ import X from './X'
 
 const NavBar = () => {
     const [show, setShow] = useState(false);
+    const image = "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
     const toggleShow = input => {
         setShow(input)
     }
@@ -23,17 +24,23 @@ const NavBar = () => {
                 </div>
                 {show && 
                 <div className="menu-container sm">
-                    <ul className="menu-ul sm">
-                        <li onClick={() => toggleShow(false)} className="menu-li sm"><Link to="/profile" style={{textDecoration: 'none', color: 'rgb(63, 63, 63)'}}>Profile</Link></li>
-                        <li onClick={() => toggleShow(false)} className="menu-li sm"><Link to="/addlisting" style={{textDecoration: 'none', color: 'rgb(63, 63, 63)'}}>List a Room</Link></li>
-                        <li onClick={() => toggleShow(false)} className="menu-li sm"><Link to="/logout" style={{textDecoration: 'none', color: 'rgb(63, 63, 63)'}}>Logout</Link></li>
+                    <ul className="menu-ul-sm sm">
+                        <Link style={{textDecoration: 'none', color: 'rgb(63, 63, 63)'}} onClick={() => toggleShow(false)} className="menu-li sm navProfileLnk" to="/profile">
+                            <img className="navbar-profileImage" src={image} /> 
+                            Profile
+                        </Link>
+                        <Link style={{textDecoration: 'none', color: 'rgb(63, 63, 63)'}} onClick={() => toggleShow(false)} className="menu-li sm" to="/addlisting">List a Room</Link>
+                        <Link style={{textDecoration: 'none', color: 'rgb(63, 63, 63)'}} onClick={() => toggleShow(false)} className="menu-li sm" to="/logout">Logout</Link>
                     </ul>
                 </div>}
                 <div className="menu-container lg">
                     <ul className="menu-ul lg">
-                        <li className="menu-li lg"><Link to="/profile" style={{textDecoration: 'none', color: '#DB00FF'}}>Profile</Link></li>
-                        <li className="menu-li lg"><Link to="/addlisting" style={{textDecoration: 'none', color: '#DB00FF'}}>List a Room</Link></li>
-                        <li className="menu-li lg"><Link to="/logout" style={{textDecoration: 'none', color: '#DB00FF'}}>Logout</Link></li>
+                        <Link className="menu-li lg navProfileLnk" to="/profile" style={{textDecoration: 'none', color: '#DB00FF'}}>
+                            <img className="navbar-profileImage" src={image} />
+                            Profile
+                        </Link>
+                        <Link className="menu-li lg" to="/addlisting" style={{textDecoration: 'none', color: '#DB00FF'}}>List a Room</Link>
+                        <Link className="menu-li lg" to="/logout" style={{textDecoration: 'none', color: '#DB00FF'}}>Logout</Link>
                     </ul>
                 </div>
             </div>
