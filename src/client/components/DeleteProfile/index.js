cd85ab0e05965d8dfd2de98fa559056afd832a78
HHSNZ <hh.stobo@gmail.com>
2021-03-22 06:31:21 +1300

    edit listing component ready-ish

--- src/client/components/DeleteProfile/index.js
+++ src/client/components/DeleteProfile/index.js
@@ -12,7 +12,7 @@ const DeleteProfile = () => {
     }
     return (
         <div className="deleteProfileWrapper">
-            <p>Are you sure you want to delete your account? You will lose all data and be signed out.</p>
+            <p>Are you sure you want to delete your account? You will lose all your data and be signed out.</p>
             <div className="buttonsWrapper">
                 <button className="button" onClick={() => goBack()}>Back</button>
                 <button className="button delBtn" onClick={() => deleteProfile(true)}>Delete</button>
--- src/client/components/EditListing/index.js
+++ src/client/components/EditListing/index.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import '../../styles/styles'
+
 
 const EditListing = () => {
+    const [listingDetails, setListingDetails] = useState(new Map()) 
+    const [images, setImages] = useState([])
+    const onChange = e => {
+        const { name, value } = e.target
+        //create a COPY of the existing map in state
+        //react will compare the two maps in useState and setState with Object.is, and trigger re-render
+        setListingDetails(new Map(listingDetails.set(name, value)))
+        console.log(`name ${name}, value ${value}`)
+    }
+    const addImages = e => {
+        setImages([...images, ...e.target.files])
+    }
+    const submit = e => {
+        e.preventDefault()
+        const formData = new FormData();
+        listingDetails.forEach((val, key) => {
+            formData.append(key, val)
+        })
+        formData.append('images', images);
+        for(var pair of formData.entries()) {
+            console.log(pair[0]+ ', '+ pair[1]);
+            if(pair[0] == 'images') {
+                console.log(`IMAGES: ...${images[1]}`)
+            }
+         } 
+        // console.log(JSON.stringify(formData))
+        // axios.put('/api/v1/listings/listing_id', formData, {
+        //     headers: {
+        //         'content-type': 'multipart/form-data'
+        //     }
+        // })
+    }
     return (
-        <div className="editListingWrapper">
-            Hello from editlisting
+        <div className="AddListingContainer editListingWrapper">
+            <div className="addListingTop">
+                <p className="small-caps-purple">Edit Listing</p>
+            </div>
+            <form className="Form addListingForm" onSubmit={submit}>
+                <input required className={`text-input ${!listingDetails.get('title') ? '' : 'lowercase'}`} type="text" name="title" placeholder="Title" value={listingDetails.get('title')} onChange={onChange} />
+                <input required className={`text-input ${!listingDetails.get('location') ? '' : 'lowercase'}`} type="text" name="location" placeholder="Location (suburb)" value={listingDetails.get('location')} onChange={onChange} />
+                <input required className={`text-input ${!listingDetails.get('rent') ? '' : 'lowercase'}`} type="number" name="rent" placeholder="Weekly rent" value={listingDetails.get('rent')} onChange={onChange} />
+                <input required className="text-input" type="number" name="bedrooms" placeholder="No. of bedrooms:" value={listingDetails.get('bedrooms')} onChange={onChange} />
+                <input required className="text-input" type="number" name="bathrooms" placeholder="No. of bathrooms:" value={listingDetails.get('bathrooms')} onChange={onChange} />
+                <div className="text-input fileContainer">
+                    <label htmlFor="file-upload-label">Upload one or more photos</label>
+                    <input required id="file-upload" className="fileUpload" type="file" multiple accept="image/png, image/jpeg" name="images" onChange={addImages}/>
+                </div>
+                <input required className="button" value="Submit" type="submit" name="submit" />
+            </form>
         </div>
     )
 }
