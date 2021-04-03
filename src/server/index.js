6695e6691d543cff105560f8ea2c7f75636ba4e8
HHSNZ <hh.stobo@gmail.com>
2021-04-04 07:19:31 +1200

    updating config for heroku

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const port = process.env.PORT
+const port = process.env.PORT || 8000
 
 app.listen(port, async function () {
  console.log(`🚀🚀🚀 App listening on port: ${port} 🚀🚀🚀`);
