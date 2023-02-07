787e2c8f4786fe66cfd2a9eaa3ab0803ad4986aa
hamish-stobo <hh.stobo@gmail.com>
2023-02-08 07:15:09 +1300

    change something to force re-deployment

--- src/client/components/DeleteListing/index.js
+++ src/client/components/DeleteListing/index.js
@@ -14,7 +14,7 @@ const DeleteListing = () => {
         const deleteResponse = await axios.delete(`/api/v1/listings/${listing_id}`)
         if(!deleteResponse || !deleteResponse?.data) throw 'Failed to delete listing'
         const { data } = deleteResponse
-        alert(`${JSON.stringify(data)} Listing was deleted.`)
+        alert(`${JSON.stringify(data)} Listing was deleted`)
         setRedirect(true)
         } catch (e) {
             alert(e)
