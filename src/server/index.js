80bf06e348736b8a8544da88b536318a5d3f496c
hamish-stobo <hh.stobo@gmail.com>
2023-05-24 06:53:22 +1000

    Add console logs when server starts

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,6 @@ const app = require('./app')
 const port = process.env.PORT || 8000
 
 app.listen(port, async function () {
-//  console.log(`🚀🚀🚀 App listening on port: ${port} 🚀🚀🚀`);
-//  console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
+ console.log(`🚀🚀🚀 App listening on port: ${port} 🚀🚀🚀`);
+ console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
 });
